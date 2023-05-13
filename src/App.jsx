e329e787c4f47a4c69ab0ce236667aaf81754e24
Ahmed Hassan <ahmedhassan@192.168.1.9>
2023-05-14 02:49:45 +0500

    Home Page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+let year = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>{year} &#9400; All Rights Reserverd</p>;
+    </footer>
+  );
+};
+export default Footer;
